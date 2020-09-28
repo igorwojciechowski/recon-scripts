ae2b4c0b61f786e9a255f8adfa3b06eb7c3a1f56
Igor Wojciechowski <igorxwojciechowski@gmail.com>
2020-09-29 00:30:06 +0200

    Add crt.sh script

---
+++ crt.sh
@@ -0,0 +1,4 @@
+# --- 
+# Searches crt.sh by specified query and retrives found urls without duplicates
+# ---
+curl -s -X GET https://crt.sh?q=$1 | grep -Po "(?<=<TD>)([a-zA-Z0-9./?=_%:-]*)(?=</TD>)" | sort -u
